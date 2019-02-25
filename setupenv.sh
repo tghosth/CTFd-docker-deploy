6b37f6e70958e6f04b4a37bfd3ceb0f0a8a493d9
Josh Grossman <tghosth@users.noreply.github.com>
2019-02-25 15:53:14 +0200

    Update setupenv.sh

--- setupenv.sh
+++ setupenv.sh
@@ -3,13 +3,13 @@ echo ""
 read -p "Before you run, check the latest compose version at: https://docs.docker.com/compose/install/"
 
 sudo apt-get remove docker docker-engine docker.io containerd runc
-sudo apt-get update -Y
+sudo apt-get update -y
 sudo apt-get install \
     apt-transport-https \
     ca-certificates \
     curl \
     gnupg-agent \
-    software-properties-common -Y
+    software-properties-common -y
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
@@ -27,8 +27,8 @@ sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
-sudo apt-get update -Y
-sudo apt-get install docker-ce docker-ce-cli containerd.io -Y
+sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 sudo usermod -aG docker ubuntu
 sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
