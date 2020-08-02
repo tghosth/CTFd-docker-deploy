458c4663de82b64bb4f6f57ea1d07626878d60aa
Josh Grossman <tghosth@users.noreply.github.com>
2020-08-02 23:24:06 +0300

    Update setupenv.sh

--- setupenv.sh
+++ setupenv.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 echo ""
 read -p "Before you run, check the latest compose version at: https://docs.docker.com/compose/install/"
+echo "First this will clone a pinned version of the CTFd repository"
+git clone -b 3.0.0 --single-branch https://github.com/CTFd/CTFd.git CTFdTEMP
+mv ./CTFdTEMP/* ./
 
 sudo apt-get remove docker docker-engine docker.io containerd runc
 sudo apt-get update -y
