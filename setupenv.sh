b064701ed1606484c866e1111f6d445b57828c61
Josh Grossman <tghosth@users.noreply.github.com>
2020-11-15 10:20:26 +0200

    Update compose version

--- setupenv.sh
+++ setupenv.sh
@@ -33,7 +33,7 @@ sudo add-apt-repository \
 sudo apt-get update -y
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 sudo usermod -aG docker ubuntu
-sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
 sudo apt-get update -y
